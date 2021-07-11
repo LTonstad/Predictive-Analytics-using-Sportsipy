238d99d8f56a2850a2b059bee87dcd4f7051c1db
LTonstad <luke.tonstad@gmail.com>
2021-07-11 17:34:07 -0600

    Auto Updated: Sun34:17 Jul 11 2021

--- src/get_sportsipy_data.py
+++ src/get_sportsipy_data.py
@@ -33,4 +33,26 @@ for game in boxscore_strings:
         print(f'DAWWWWW {game} did not get a boxscore')
         continue
 
+# Adding additional columns
+
+# String of full day of the week (Sunday, Monday, Thursday)
+df_boxscore['day_of_week'] = df_boxscore['date'].str.split(expand=True)[0]
+# Abbreviation of month as a string
+df_boxscore['month'] = df_boxscore['date'].str.split(expand=True)[1]
+# Temperature as integer in Farenheight
+df_boxscore['temperature'] = df_boxscore['weather'].str.split(',', expand=True)[0].str.split(expand=True)[0]
+# Relative humidity as a percentage
+df_boxscore['humidity'] = df_boxscore['weather'].str.split(',', expand=True)[1].str.split(expand=True)[2]
+# Wind chill as integer
+df_boxscore['wind_chill'] = df_boxscore['weather'].str.split(',', expand=True)[3].str.split(expand=True)[2]
+
+# Wind in MPH, takes a few extra steps
+df_boxscore['wind'] = df_boxscore['weather'].str.split(',', expand=True)[2]
+# Converts any games played in a dome to 0, originally listed as NoneType
+team_data['wind'] = team_data['wind'].apply(lambda x: 0 if x is None else x)
+# Converts any games listed as 'no wind' to 0
+team_data['wind'] = team_data['wind'].apply(lambda x: 0 if str(x)[-1:] == 'd' else x)
+# Changing all values to the integers
+team_data['wind'] = team_data['wind'].str.split(expand=True)[1].fillna(0).astype('int64')
+
 df_boxscore.to_pickle('../data/df_boxscores.pkl')
